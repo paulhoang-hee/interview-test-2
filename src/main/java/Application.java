6498ceca4f659b58e40cf070c6a4bf2efb8fac38
Paul Hoang <paul.hoang@hee.nhs.uk>
2018-02-27 16:57:02 +0000

    initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,16 @@
+/**
+ * Task:
+ *
+ * You have been given a list of words and for each one, you are required to print a statement to indicate if the word is or
+ * isn't a palindrome (a word when spelt backward is the same as spelt normally)
+ *
+ * You have full autononmy here. Add any libraries you need, google as much as you want, ask any questions
+ * to clear up any unknowns. Just code as if you are at work, delivering high quality code.
+ */
+public class Application {
+
+  public static void main(String[] args) {
+    
+  }
+
+}
